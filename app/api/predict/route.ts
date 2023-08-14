1d6af193a7138c75bc463e9b0fb207215efeb611
Jackson Ndiang'ui <jacksongathondu@gmail.com>
2023-08-14 20:39:46 +0300

    routes

---
+++ app/api/predict/route.ts
@@ -0,0 +1,25 @@
+import { S3 } from "aws-sdk";
+import { client } from "@gradio/client";
+import { NextResponse } from "next/server";
+
+const s3 = new S3({
+    apiVersion: "2006-03-01",
+    accessKeyId: process.env.ACCESS_KEY,
+    secretAccessKey: process.env.SECRET_KEY,
+    region: process.env.REGION,
+    signatureVersion: "v4",
+  });
+export async function POST(req: Request) {
+
+    const { Key } = await req.json() 
+    console.log("key", Key)
+    const imageUrl = await s3.getSignedUrlPromise("getObject", {
+        Bucket: process.env.BUCKET_NAME,
+        Key: Key
+      });
+ 
+
+     
+      return  NextResponse.json({imageUrl})
+      
+}
---
+++ app/api/upload/route.ts
@@ -0,0 +1,40 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import S3 from "aws-sdk/clients/s3";
+import { nanoid } from "nanoid";
+import { NextResponse } from "next/server";
+
+
+const s3 = new S3({
+  apiVersion: "2006-03-01",
+  accessKeyId: process.env.ACCESS_KEY,
+  secretAccessKey: process.env.SECRET_KEY,
+  region: process.env.REGION,
+  signatureVersion: "v4",
+});
+
+
+export async function GET() {
+  const Key = nanoid();
+
+  try {
+
+
+     
+
+      const s3Params = {
+        Bucket:process.env.BUCKET_NAME,
+        Key,
+        Expires: 60,
+      
+      };
+      const uploadUrl = await s3.getSignedUrlPromise("putObject", s3Params);
+    return  NextResponse.json({
+        uploadUrl,
+        key: Key,
+      });
+    
+  
+  } catch (error) {
+    console.log(error);
+  }
+}
