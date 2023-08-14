7d6f1c6e5694fa24e61b9d90860b94a773be8972
Jackson Ndiang'ui <jacksongathondu@gmail.com>
2023-08-14 20:39:14 +0300

    config for gradio

--- next.config.js
+++ next.config.js
@@ -1,4 +1,33 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const {webpack} = require("next/dist/compiled/webpack/webpack");
+const nextConfig = {
+
+    webpack: (config, {isServer}) => {
+        if (!isServer) {
+            config.resolve = {
+                ...config.resolve,
+                fallback: {
+                    // fixes proxy-agent dependencies
+                    net: false,
+                    dns: false,
+                    tls: false,
+                    assert: false,
+                    // fixes next-i18next dependencies
+                    path: false,
+                    fs: false,
+                    // fixes mapbox dependencies
+                    events: false,
+                    // fixes sentry dependencies
+                    process: false
+                }
+            };
+        }
+        config.plugins.push(new webpack.NormalModuleReplacementPlugin(/node:/, (resource) => {
+            resource.request = resource.request.replace(/^node:/, "");
+        }))
+
+        return config
+    },
+}
 
 module.exports = nextConfig
